390c1c7ccd4ae8d3c1f271073d0aa7d803dc8ac2
bountyvmodels <kyray.smith.agent@gmail.com>
2025-06-19 01:26:04 -0400

    Update page.tsx

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,7 +1,6 @@
 // ui/app/page.tsx
 import dynamic from 'next/dynamic';
 
-// Disable SSR
 const LandingClient = dynamic(() => import('./LandingClient'), { ssr: false });
 
 export default function HomePage() {
